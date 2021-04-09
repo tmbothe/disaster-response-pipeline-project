f60e633b73a8eb9e7b2bd13148aafe2d78da7d0a
tmbothe <mbothe7@hotmail.com>
2021-04-09 15:39:11 -0400

    buiding classify file

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,94 @@
 import sys
+import numpy as np
+import pandas as pd
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+from sklearn.pipeline import Pipeline,FeatureUnion
+from sklearn.metrics import confusion_matrix, classification_report, accuracy_score,fbeta_score
+from sklearn.model_selection import train_test_split,GridSearchCV
+from sklearn.ensemble import RandomForestClassifier,AdaBoostClassifier
+from sklearn.base import BaseEstimator,TransformerMixin
+from sklearn.feature_extraction.text import CountVectorizer,TfidfTransformer
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.naive_bayes import MultinomialNB
+from nltk.corpus import stopwords
+from sqlalchemy import create_engine 
+import pickle
+import warnings
+warnings.filterwarnings('always', 'ResourceWarning') 
+from data import process_data
 
 
 def load_data(database_filepath):
-    pass
+    """This function read data from a sqlite table and returns two dataframes
+
+    Args:
+        database_filepath (string): [file pattrain_classifierh to the sqllite table]
+
+    Returns:
+        [X]: [dataframe with the text data as features]
+        [Y]: [dataframe with response variable; all categories]
+    """
+    engine = create_engine('sqlite:///database_filepath.db')
+    df = pd.read_sql_table('database_filepath',con=engine)
+
+    X = df.message
+    Y = df.iloc[:,4:]
+    return X , Y
 
 
 def tokenize(text):
-    pass
+    """ This function takes text as input, then process all the transformations necessary
+       to tokenize, lemmatize and returns a list with just tokens
+
+    Args:
+        text ([string]): [text that needs to be tokenized]
+    """
+    tokens = word_tokenize(text)
+    tokens = [w for w in tokens if w not in stopwords.words('english')]
+    lemmatizer = WordNetLemmatizer()
+    
+    clean_tokens = []
+    for token in tokens:
+        
+        clean_token=lemmatizer.lemmatize(token).lower().strip()
+        clean_tokens.append(clean_token)
+    return clean_tokens
 
 
 def build_model():
-    pass
+    """This function builds a model to successively tokenize, transform and fit data
+
+    Returns:
+        [model]: [model]
+    """
+    pipeline = Pipeline([
+        ('vect',CountVectorizer(tokenizer=tokenize)),
+        ('tfidf',TfidfTransformer()),
+        ('clf',MultiOutputClassifier(RandomForestClassifier()))
+        ])
+    
+    return pipeline
 
 
-def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+def evaluate_model(model, X_test, y_test, category_names):
+    """Print classification report for positive labels"""
+
+    y_pred = model.predict(X_test)
+    print(classification_report(y_test, y_pred, target_names=category_names))
+    print("Fbeta score:", fbeta_score(y_test, y_pred, beta=2, average="weighted"))
 
 
 def save_model(model, model_filepath):
-    pass
+    """Save the final model
+
+    Args:
+        model ([string]): model name 
+        model_filepath ([string]): file path where the model is save
+    """
+    pickle.dump(model, open(model_filepath, "wb"))
+    
 
 
 def main():
